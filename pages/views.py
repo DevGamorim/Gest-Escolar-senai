9e209ad612288ab1f0783fc7b2aa8d90b89ea174
Fernando Amorim <site3@kaolaeletroshop.com.br>
2021-11-19 15:14:15 -0300

    cadastro pessoa

--- pages/views.py
+++ pages/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.hashers import make_password, check_password
 # Create your views here.
 
 #Models banco
-from table.models import user
+from table.models import user, endereco, pessoa
 
 # Bibliotecas externas
 
@@ -17,25 +17,18 @@ class HomePageView(TemplateView):
     template_name = 'home.html'
 
 def criar_user(request):
-    '''
-        password
-        id_superuser
-        username
-        first_name
-        last_name
-        email
-        is_staff
-        is_active
-        privilegio
-
-    '''
     if request.method == 'POST':
+        # salva todas informações do usuario em um array
         tudo = request.POST.copy()
         tudo = json.dumps(tudo)
         tudo = json.loads(tudo)
+
+        #criptografa a senha do novo usuario
         senha_cripto = make_password(password=tudo['password'], salt=None, hasher='pbkdf2_sha256')
+
+        #criptografa a senha do novo usuario
         if tudo['privilegio'] == "adm":
-            create = user.objects.create(password=senha_cripto,
+            usuario = user.objects.create(password=senha_cripto,
             username = tudo['username'],
             first_name = tudo['first_name'],
             last_name = tudo['last_name'],
@@ -45,7 +38,7 @@ def criar_user(request):
             privilegio =tudo['privilegio']
             )
         else:
-            create = user.objects.create(password=senha_cripto,
+            usuario = user.objects.create(password=senha_cripto,
             username = tudo['username'],
             first_name = tudo['first_name'],
             last_name = tudo['last_name'],
@@ -54,7 +47,22 @@ def criar_user(request):
             is_active =1,
             privilegio =tudo['privilegio']
             )
-        print("ok")
+        
+        endereco_ = endereco.objects.create(
+            bairro = tudo['bairro'],
+            rua = tudo['rua'],
+            numero = tudo['numero'],
+            complemento = tudo['complemento']
+        )
+
+        pessoa_ = pessoa.objects.create(
+            nome = str(tudo['first_name'])+" "+str(tudo['last_name']),
+            cpf = tudo['cpf'],
+            nascimento = tudo['nascimento'],
+            endereco = endereco_,
+            usuario = usuario
+        )
+        print("create")
     else:
         print("nbbbbb")
     return render(request,'perfil/criar_perfil.html')
